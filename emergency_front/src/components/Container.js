5319e1098280f11357f5fd3339e6780bcaab172c
Junikarp <zieunpk0924@gmail.com>
2023-12-28 19:57:22 +0900

    feat : allergy 입력 추가

--- emergency_front/src/components/Container.js
+++ emergency_front/src/components/Container.js
@@ -1,9 +1,12 @@
 const Container = ({ children }) => {
   const footerContainer = {
     backgroundColor: "#141E46",
+    width: "100%",
     padding: "0.7vh 0.7vw",
     display: "flex",
     justifyContent: "space-around",
+    position: "fixed",
+    bottom: 0,
   };
   return <div style={footerContainer}>{children}</div>;
 };
--- emergency_front/src/components/pages/MyPage/SearchAllergy.js
+++ emergency_front/src/components/pages/MyPage/SearchAllergy.js
@@ -1,91 +1,155 @@
 import React, { useState, useEffect } from 'react';
 import Select from 'react-select';
 import DatePicker from 'react-datepicker';
+import FooterNav from "../../FooterNav";
+import { jwtDecode } from "jwt-decode";
+import { useNavigate } from "react-router-dom";
 import 'react-datepicker/dist/react-datepicker.css';
+import './SearchAllergy.css'
 
 const MakeSelect = () => {
-  const [selectedDisease, setSelectedDisease] = useState(null); // 선택된 알레르기 상태
-  const [selectedDate, setSelectedDate] = useState(new Date()); // 선택된 날짜 상태
-  const [diseaseOptions, setDiseaseOptions] = useState([]); // 알레르기 옵션 목록 상태
+  const [selectedDisease, setSelectedDisease] = useState(null);
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [diseaseOptions, setDiseaseOptions] = useState([]);
+  const [userInfo, setUserInfo] = useState({ id: "", name: "" }); // 사용자 정보 상태 추가
+  const [Allergy, setAllergy] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    // 컴포넌트가 마운트될 때 한 번만 실행되는 부분
-    fetch('http://localhost:8080/disease-api/mypage/selectAllergy')
-      .then(response => response.json()) // 응답을 JSON 형태로 변환
-      .then(data => {
-        // 받은 데이터를 Select에서 사용할 형태로 변환
-        const options = data.map(allergyName => ({
-          value: allergyName.aid,
-          label: allergyName.aname
-        }));
-        setDiseaseOptions(options); // 변환된 옵션 목록을 상태에 저장
-      })
-      .catch(error => console.error('Error fetching data:', error));
-  }, []); // 빈 배열을 두 번째 인자로 전달하여 한 번만 실행되도록 함
+    const fetchData = async () => {
+      try {
+        const existToken = localStorage.getItem("token");
+        if (existToken) {
+          const userToken = jwtDecode(existToken);
+          const decodedId = userToken.id;
+          setUserInfo({ id: decodedId }); // ID 설정
+
+          // Fetch user name from backend API
+          const response = await fetch(`http://localhost:8080/api/user/${decodedId}`);
+          if (response.ok) {
+            const userData = await response.json();
+            setUserInfo(prevState => ({ ...prevState, name: userData.name })); // 이름 설정
+          } else {
+            console.error('Error fetching user data:', response.statusText);
+          }
+
+          // Fetch allergy data
+          const allergyResponse = await fetch('http://localhost:8080/disease-api/mypage/selectAllergy');
+          if (allergyResponse.ok) {
+            const allergyData = await allergyResponse.json();
+            const options = allergyData.map(allergyName => ({
+              value: allergyName.aid,
+              label: allergyName.aname
+            }));
+            setDiseaseOptions(options);
+          } else {
+            console.error('Error fetching allergy data:', allergyResponse.statusText);
+          }
+
+
+          // Fetch user's allergy info
+          const userAllergyResponse = await fetch(`http://localhost:8080/disease-api/mypage/${decodedId}/allergy`);
+          if (userAllergyResponse.ok) {
+            const userAllergyData = await userAllergyResponse.json();
+            setAllergy(userAllergyData);
+          } else {
+            console.error('Error fetching user allergy data:', userAllergyResponse.statusText);
+          }
+        }
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+
+
+    fetchData();
+  }, []);
+
+
 
   const handleDiseaseChange = selectedOption => {
-    setSelectedDisease(selectedOption); // 선택된 알레르기를 상태에 저장
+    setSelectedDisease(selectedOption);
   };
 
   const handleDateChange = date => {
-    setSelectedDate(date); // 선택된 날짜를 상태에 저장
+    setSelectedDate(date);
   };
 
-  const handleSave = () => {
-
+  const handleSave = async () => {
     const year = selectedDate.getFullYear();
-    const month = String(selectedDate.getMonth() + 1).padStart(2, '0'); // 월은 0부터 시작하므로 1을 더하고 두 자리로 표시
-    const day = String(selectedDate.getDate()).padStart(2, '0'); // 일은 두 자리로 표시
-
+    const month = String(selectedDate.getMonth() + 1).padStart(2, '0');
+    const day = String(selectedDate.getDate()).padStart(2, '0');
     const formattedDate = `${year}-${month}-${day}`;
 
-    // 서버로 데이터 전송하는 로직
     const disease = {
-      userId: "ssafy",
+      userId: userInfo.id,
       category: "allergy",
       value: selectedDisease.label,
       diseaseDate: formattedDate
     };
-    fetch('http://localhost:8080/disease-api/mypage', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(disease)
-    })
-      .then(response => response.json()
-      )
-      .then(data => {
-        // 서버로부터의 응답 처리
-        console.log('Success:', data);
-        alert("등록 완료!")
-      })
-      .catch(error => {
-        console.error('Error:', error);
+
+    try {
+      const response = await fetch('http://localhost:8080/disease-api/mypage', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(disease)
       });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log('Save successful:', data);
+        alert("등록 완료!");
+      } else {
+        console.error('Save failed:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error saving data:', error);
+    }
   };
+
+  if (!userInfo.id) {
+    return <div>Loading...</div>; // 유저 정보가 없으면 로딩 중을 표시합니다.
+  }
 
   return (
     <>
-      <p>알레르기 선택</p>
-      <Select
-        value={selectedDisease} // 선택된 알레르기 상태를 값으로 설정
-        onChange={handleDiseaseChange} // 선택된 알레르기를 상태에 저장하는 함수
-        placeholder="알레르기 원인을 선택하세요."
-        options={diseaseOptions} // 알레르기 옵션 목록을 설정
-      />
-      <p>날짜 선택</p>
-      <DatePicker
-        selected={selectedDate} // 선택된 날짜 상태를 값으로 설정
-        onChange={handleDateChange} // 선택된 날짜를 상태에 저장하는 함수
-      />
-      <button onClick={handleSave}>저장</button>
-      {selectedDisease && ( // 선택된 알레르기가 있을 때만 출력
-        <div>
-          <p>선택된 알레르기: {selectedDisease.label}</p>
-          <p>선택된 날짜: {selectedDate.toLocaleDateString()}</p>
+      <div id="name-box">
+        {userInfo.name} 님 {/* 이름과 ID를 표시합니다. */}
+      </div>
+      <div id="main-con">
+        <div id="input-box">
+          <div>
+            <p>알레르기 선택</p>
+            <Select id="select"
+              value={selectedDisease}
+              onChange={handleDiseaseChange}
+              placeholder="알레르기 원인을 선택하세요."
+              options={diseaseOptions}
+            />
+          </div>
+          <div>
+            <p>날짜 선택</p>
+            <DatePicker id="picker"
+              selected={selectedDate}
+              onChange={handleDateChange}
+            />
+            <button className="save" onClick={handleSave}>저장</button>
+          </div>
         </div>
-      )}
+        <div>
+          <ul>
+        {/* Disease 배열을 순회하며 목록을 출력합니다. */}
+        {Allergy.map((item, index) => (
+          <li key={index}>{item.value} {item.diseaseDate}</li>
+        ))}
+      </ul>
+        </div>
+      </div>
+      <div className="foot">
+        <FooterNav />
+      </div>
     </>
   );
 };
