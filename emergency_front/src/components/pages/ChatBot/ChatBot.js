0ac0d3ced833228d8050d66c6b9bc0ce3130f0d5
김은지 <eungg0214@gmail.com>
2023-12-30 17:19:51 +0900

    feat:chatbot css

--- emergency_front/src/components/pages/ChatBot/ChatBot.js
+++ emergency_front/src/components/pages/ChatBot/ChatBot.js
@@ -1,7 +1,13 @@
 import FooterNav from "../../../components/FooterNav";
+import { Link } from "react-router-dom";
 import { useState } from "react";
 import { CallGPT } from "../../api/ChatGpt";
 import ChatBotInput from "./ChatBotInput";
+import menu from "../../../assets/menubar.png";
+import firstaid from "../../../assets/firstaid.png";
+import chatbot from "../../../assets/chatbot.png";
+import home from "../../../assets/home.png";
+import mypage from "../../../assets/mypage.png";
 import "./ChatBot.css";
 const apiKey = process.env.REACT_APP_GPT_API_KEY;
 // const dummyData = `{"respond": "이가 빠진 경우, 즉시 다음과 같은 조치를 취해야 합니다:
@@ -9,6 +15,10 @@ const apiKey = process.env.REACT_APP_GPT_API_KEY;
 //   2. 두 손으로 치아를 잡아 부모님 또는 보호자께서 치과 의사를 방문할 수 있도록 보관해주세요.
 //   3. 치과 의사에게 즉시 연락하여 긴급한 상황임을 알려주세요.\n\n치과 의사의 전문적인 도움이 필요하기 때문에 가능한 빨리 전문가의 도움을 받을 수 있도록 하세요."}`;
 const ChatBot = () => {
+  const imgStyle = {
+    width: "30px",
+    height: "30px",
+  };
   const [data, setData] = useState("");
   // const [userChat, setUserChat] = useState([]);
   // const [gptChat, setGptChat] = useState([]);
@@ -20,16 +30,41 @@ const ChatBot = () => {
     try {
       setIsLoading(true);
       //message는 문자열로 받고
-      const message = await CallGPT(apiKey, {
-        prompt: `${userInput}`,
-      });
-      console.log(message);
-      //setData 해줄때는 json으로 해주기
-      setData(JSON.parse(message));
-      //사용자 input저장하기
+      //여기는 배열에 한번에 저장해서 상태 출력해서 내 input이랑 gpt응답이 같이 뜸
+      //     const message = await CallGPT(apiKey, {
+      //       prompt: `${userInput}`,
+      //     });
+      //     console.log(message);
+      //     //setData 해줄때는 json으로 해주기
+      //     setData(JSON.parse(message));
+      //     //사용자 input저장하기
+      //     setChat((prevChat) => [
+      //       ...prevChat,
+      //       { role: "user", content: userInput },
+      //       { role: "gpt", content: JSON.parse(message).respond },
+      //     ]);
+      //   } catch (error) {
+      //     console.log(error);
+      //   } finally {
+      //     setIsLoading(false);
+      //   }
+      // };
+
+      //-----------------------------------
+      // 사용자 입력 메시지를 먼저 추가
       setChat((prevChat) => [
         ...prevChat,
         { role: "user", content: userInput },
+      ]);
+
+      // GPT 응답 받아오기
+      const message = await CallGPT(apiKey, {
+        prompt: `${userInput}`,
+      });
+
+      // GPT 응답 메시지 추가
+      setChat((prevChat) => [
+        ...prevChat,
         { role: "gpt", content: JSON.parse(message).respond },
       ]);
     } catch (error) {
@@ -48,20 +83,45 @@ const ChatBot = () => {
     <>
       <header className="chatbot-header">
         <h2>이응</h2>
+        <div>
+          <button className="chatbot-header-menubar">
+            <img src={menu} style={imgStyle} />
+          </button>
+          <div className="chatbot-menubar-dropdown">
+            <Link to="/">
+              <img src={home} style={imgStyle} />
+            </Link>
+            <Link to="/firstaid">
+              <img style={imgStyle} src={firstaid}></img>
+            </Link>
+            <Link to="/mypage">
+              <img style={imgStyle} src={mypage}></img>
+            </Link>
+          </div>
+        </div>
       </header>
-      <div>
+      <div className="chatbot-message-container">
         {chat.map((message, index) => (
-          <div
-            key={index}
-            className={message.role === "user" ? "userChat" : "gptChat"}
-          >
-            {message.content}
+          <div key={index}>
+            {message.role === "user" ? (
+              <div className="userChat">
+                <div className="chatbot-message">
+                  <p>{message.content}</p>
+                </div>
+              </div>
+            ) : (
+              <div className="gptChat">
+                <div className="chatbot-message">
+                  <p>{message.content}</p>
+                </div>
+              </div>
+            )}
           </div>
         ))}
       </div>
       {/* prop 보내주기 => input한테! isLoading이랑 onSubmit*/}
       {/* <div>data : {data?.respond}</div> */}
-      <div>isLoading : {isLoading ? "loading.." : "fin"}</div>
+      {/* <div>isLoading : {isLoading ? "loading.." : "fin"}</div> */}
       <div className="chatbot-input">
         <ChatBotInput isLoading={isLoading} onSubmit={handleSubmit} />
       </div>
--- emergency_front/src/components/pages/ChatBot/ChatBotInput.js
+++ emergency_front/src/components/pages/ChatBot/ChatBotInput.js
@@ -1,29 +1,48 @@
 import { useState } from "react";
 import "./ChatBotInput.css";
-
+import send from "../../../assets/sendbutton.png";
 //isLoading 받아오기(ChatBot에서 내려준 prop)
 const ChatBotInput = ({ onSubmit }) => {
+  const imgStyle = {
+    width: "42px",
+    height: "42px",
+  };
   const [userInput, setUserInput] = useState("");
   //사용자 입력 받아서 상위 컴포넌트로 데이터 전달
   //loading 상태에서 사용자가 제출버튼 못 누르도록 처리
   const handleUserInput = (e) => {
     setUserInput(e.target.value);
   };
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      handleClick();
+    }
+  };
   const handleClick = () => {
     //Chatbot으로 올려준다
     onSubmit(userInput);
     setUserInput("");
   };
+
   return (
     <>
       <div className="userInputContainer">
-        <textarea
-          className="styles.userInputText"
-          value={userInput}
-          onChange={handleUserInput}
-          placeholder="궁금한 응급처치 방법을 물어보세요"
-        />
-        <button onClick={handleClick}>알려줘잉</button>
+        <div className="userInput-textarea">
+          <textarea
+            className="userInputText"
+            value={userInput}
+            onChange={handleUserInput}
+            // enter 눌러도 요청 보내짐
+            onKeyDown={handleKeyDown}
+            placeholder="궁금한 응급처치 방법을 물어보세요"
+          />
+        </div>
+        <div className="userInput-button">
+          <button onClick={handleClick}>
+            <img style={imgStyle} src={send} />
+          </button>
+        </div>
       </div>
     </>
   );
